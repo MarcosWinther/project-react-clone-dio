f1defbf7410f0262c86a36b9478cccb99f26bcc8
MarcosWinther <winthermarcos@gmail.com>
2023-11-28 21:40:39 -0300

    Construção dos componentes

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-
 import { Link } from 'react-router-dom';
+
+import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
 
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
-      <Link to="/login">Fazer login</Link>
+      <Header />
     </>
   )
 }
